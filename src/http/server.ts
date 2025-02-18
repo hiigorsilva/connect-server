35ea15c250a7c501421acbc9186c9c43e2dd895a
hiigorsilva <higorscontato@gmail.com>
2025-02-18 16:54:09 -0300

    feat: create subscribe to event route

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,11 +1,15 @@
 import { fastifyCors } from '@fastify/cors'
+import { fastifySwagger } from '@fastify/swagger'
+import { fastifySwaggerUi } from '@fastify/swagger-ui'
+import { fastify } from 'fastify'
 import {
   type ZodTypeProvider,
+  jsonSchemaTransform,
   serializerCompiler,
   validatorCompiler,
 } from 'fastify-type-provider-zod'
-
-import { fastify } from 'fastify'
+import { env } from '../env'
+import { subscribeToEventRoute } from '../routes/subscription-to-event'
 
 const app = fastify({ logger: true }).withTypeProvider<ZodTypeProvider>()
 
@@ -14,10 +18,22 @@ app.setSerializerCompiler(serializerCompiler)
 
 app.register(fastifyCors)
 
-app.get('/subscriptions', async (_request, reply) => {
-  reply.status(200).send({ message: 'Hello World' })
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: 'Connect API',
+      version: '0.0.1',
+    },
+  },
+  transform: jsonSchemaTransform,
 })
 
-app.listen({ port: 3333 }).then(() => {
-  console.log('HTTP server running on http://localhost:3333')
+app.register(fastifySwaggerUi, {
+  routePrefix: '/docs',
+})
+
+app.register(subscribeToEventRoute)
+
+app.listen({ port: env.PORT }).then(() => {
+  console.log(`HTTP server running on port ${env.PORT}`)
 })
